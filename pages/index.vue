b892bc9f9aab9ece4e70aee9264a9bfcb855410e
Vittor de Aguiar <vittoraguiar93@gmail.com>
2024-06-27 10:19:29 -0300

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="w-screen flex flex-col xl:pb-0 absolute">
+    <section class="flex flex-row flex-grow h-full">
+      <HelloWorld />
+    </section>
+  </main>
+</template>
+
+<style>
+.debug {
+  border: 2px solid red;
+  border-radius: 4px;
+}
+</style>
